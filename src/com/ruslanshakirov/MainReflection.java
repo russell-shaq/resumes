f6ae5532a4bc18d154204814f0912dfdd97408ba
Ruslan <shakirovusa@gmail.com>
2019-03-09 04:52:42 +0600

    Implemented MainReflection

--- src/com/ruslanshakirov/MainReflection.java
+++ src/com/ruslanshakirov/MainReflection.java
@@ -1,4 +1,25 @@
 package com.ruslanshakirov;
 
+import com.ruslanshakirov.model.Resume;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 public class MainReflection {
+
+    public static void main(String[] args) throws IllegalAccessException, NoSuchMethodException, InvocationTargetException {
+        Resume r = new Resume();
+        Class resumeClass = r.getClass();
+        Field field = resumeClass.getDeclaredFields()[0];
+        field.setAccessible(true);
+        System.out.println(field.getName());
+        System.out.println(field.get(r));
+        field.set(r, "new_uuid");
+        // TODO : invoke r.toString via reflection
+        Method toString = resumeClass.getDeclaredMethod("toString");
+        String invoke = (String) toString.invoke(r);
+        System.out.println(r);
+        System.out.println(invoke);
+    }
 }
