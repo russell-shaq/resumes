3397a6ce2bae55bf7c6f8e76fb0408f18f7a6053
Ruslan <shakirovusa@gmail.com>
2019-03-11 15:12:00 +0600

    Implemented MapStorage

--- src/com/ruslanshakirov/storage/MapStorage.java
+++ src/com/ruslanshakirov/storage/MapStorage.java
@@ -1,4 +1,50 @@
 package com.ruslanshakirov.storage;
 
-public class MapStorage {
+import com.ruslanshakirov.model.Resume;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapStorage extends AbstractStorage {
+    protected Map<String, Resume> storage = new HashMap<String, Resume>();
+
+    @Override
+    public void clear() {
+        storage.clear();
+    }
+
+    @Override
+    public int size() {
+        return storage.size();
+    }
+
+    @Override
+    protected int getIndex(String uuid) {
+        return storage.containsKey(uuid) ? 1 : -1;
+    }
+
+    @Override
+    protected Resume doGet(String uuid, int index) {
+        return storage.get(uuid);
+    }
+
+    @Override
+    protected void doSave(Resume resume, int index) {
+        storage.put(resume.getUuid(), resume);
+    }
+
+    @Override
+    protected void doUpdate(Resume resume, int index) {
+        storage.put(resume.getUuid(), resume);
+    }
+
+    @Override
+    protected void doDelete(String uuid, int index) {
+        storage.remove(uuid);
+    }
+
+    @Override
+    protected Resume[] doGetAll() {
+        return storage.values().toArray(new Resume[0]);
+    }
 }
