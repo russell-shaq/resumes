50309430168de399a3c8e3f770e03cf938768409
Ruslan <shakirovusa@gmail.com>
2019-03-11 15:11:31 +0600

    Implemented ListStorage

--- src/com/ruslanshakirov/storage/ListStorage.java
+++ src/com/ruslanshakirov/storage/ListStorage.java
@@ -1,4 +1,54 @@
 package com.ruslanshakirov.storage;
 
-public class ListStorage {
+import com.ruslanshakirov.model.Resume;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListStorage extends AbstractStorage {
+    private List<Resume> storage = new ArrayList<>();
+
+    protected int getIndex(String uuid) {
+        for (int i = 0; i < storage.size(); i++) {
+            if (storage.get(i).getUuid().equals(uuid)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    @Override
+    protected Resume doGet(String uuid, int index) {
+        return storage.get(index);
+    }
+
+    @Override
+    protected void doSave(Resume resume, int index) {
+        storage.add(resume);
+    }
+
+    @Override
+    protected void doUpdate(Resume resume, int index) {
+        storage.set(index, resume);
+    }
+
+    @Override
+    protected void doDelete(String uuid, int index) {
+        storage.remove(index);
+    }
+
+    @Override
+    protected Resume[] doGetAll() {
+        return storage.toArray(new Resume[0]);
+    }
+
+    @Override
+    public void clear() {
+        storage.clear();
+    }
+
+    @Override
+    public int size() {
+        return storage.size();
+    }
 }
